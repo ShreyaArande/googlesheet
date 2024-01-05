97220f33ab9ce846cb29fa79fe6b86439eb84a58
otobit-shreya <arshreya@otobit.in>
2024-01-05 15:32:28 +0530

    google sheet api

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-preview',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './preview.component.html',
-  styleUrl: './preview.component.css'
+  styleUrl: './preview.component.css',
 })
-export class PreviewComponent {
+export class PreviewComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  range = 'Sheet1';
+  spreadsheetId = '16KFhONQK9W4_-rOTWlXPyZYAe4HyAwOy4Xyvvl2ldB8';
+  apiKey = 'AIzaSyCg8Xa6Kqq4ziNmjDHBlfTPiFqNhHJFGME';
+  data:any
 
+  getData() {
+    this.http
+      .get(
+        `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${this.range}?key=${this.apiKey}`
+      )
+      .subscribe(
+        (res) => {
+          console.log(res, 'response from sheet');
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
 }
