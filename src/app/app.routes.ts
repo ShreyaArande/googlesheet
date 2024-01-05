e1313d957d48b7834733c7921afa255443598ac0
Shreya Arande <arshreya@otobit.in>
2024-01-05 13:05:39 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { PreviewComponent } from './preview/preview.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PreviewComponent },
+
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
